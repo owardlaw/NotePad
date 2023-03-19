cb4e2a11ce50354b711b96a689763bc629796568
Owen Wardlaw <owardlaw@ucsd.edu>
2023-03-19 13:00:51 -0700

    Removing multi note code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
 import NotePad from "./components/NotePad";
-import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
 const App = () => {
-  // const [noteId, setNoteId] = useState(0);
-
-  // function nextNote() {
-  //   setNoteId((prevNoteId) => prevNoteId + 1);
-  // }
-
-  // function prevNote() {
-  //   setNoteId((prevNoteId) => (prevNoteId > 0 ? prevNoteId - 1 : 0));
-  // }
-
   return (
     <div className="App">
-      {/* <div className="arrows">
-        <HiChevronLeft id="arrow" onClick={prevNote} />
-        <p style={{color:"white"}}>{noteId}</p>
-        <HiChevronRight id="arrow" onClick={nextNote} />
-      </div>
-      <NotePad id={noteId} /> */}
       <NotePad />
     </div>
   );
